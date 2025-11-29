13cb1b0afb64a0357f8be9b7219e9c8d8a3b8c25
Fahad <fahadbinhussain001@gmail.com>
2025-11-29 11:41:49 +0600

    feat: Add main entry point and routing setup with initial HTML structure for ImgVault

---
+++ nextgen-extension/src/index.jsx
@@ -0,0 +1,27 @@
+/**
+ * @fileoverview Main Entry Point with Router
+ * @version 2.0.0
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
+import GalleryPage from './pages/GalleryPage';
+import SettingsPage from './pages/SettingsPage';
+import TrashPage from './pages/TrashPage';
+import PopupPage from './pages/PopupPage';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<GalleryPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/trash" element={<TrashPage />} />
+        <Route path="/popup" element={<PopupPage />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </HashRouter>
+  </React.StrictMode>
+);
